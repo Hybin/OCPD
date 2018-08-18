bfee307c883794005af1f1424d2b398f145680e2
Hybin <hhb8550@live.com>
2018-08-18 15:52:27 +0800

    New Model[Lexicon]

--- app/Models/Lexicon.php
+++ app/Models/Lexicon.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Lexicon extends Model
 {
-    //
+    use SoftDeletes;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'lexicons';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['cn_character', 'phonetic_element', 'rhyme_element', 'reconstruction_wl', 'reconstruction_lfg', 
+                           'reconstruction_byp', 'reconstruction_byps', 'reconstruction_zzsf', 'traditional_pronunciation', 
+                           'rhythm_status', 'guangyun_position', 'modern_pronunciation'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
--- database/migrations/2018_08_09_151617_create_lexicons_table.php
+++ database/migrations/2018_08_09_151617_create_lexicons_table.php
@@ -15,7 +15,20 @@ class CreateLexiconsTable extends Migration
     {
         Schema::create('lexicons', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('cn_character');            // 汉字
+            $table->string('phonetic_element');      // 声符
+            $table->string('rhyme_element');      // 韵部
+            $table->string('reconstruction_wl');           // 拟音-王力
+            $table->string('reconstruction_lfg');          // 拟音-李方桂
+            $table->string('reconstruction_byp');          // 拟音-白一平
+            $table->string('reconstruction_byps');         // 拟音-白一平——沙加尔
+            $table->string('reconstruction_zzsf');         // 拟音-郑张尚芳
+            $table->string('traditional_pronunciation');         // 广韵反切
+            $table->string('rhythm_status');        // 音韵地位
+            $table->string('guangyun_position');          // 广韵位置
+            $table->string('modern_pronunciation');         // 今读
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
