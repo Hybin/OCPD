72914fd23f6c0cd828c2859ea6d5e24e5b0739ba
Hybin <hhb8550@live.com>
2018-08-23 21:20:39 +0800

    Users Managemant

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
 
 class SessionsController extends Controller
 {
-    //
+	public function __construct()
+	{
+		$this->middleware('guest', [
+			'only' => ['create']
+		]);
+	}
+	
+	public function create()
+	{
+		return view('sessions.create');
+	}
+	
+	public function store(Request $request)
+	{
+		$credentials = $this->validate($request, [
+			'email' => 'required|email|max:255',
+			'password' => 'required'
+		]);
+
+		if (Auth::attempt($credentials, $request->has('remember'))) {
+			return redirect()->intended(route('home'));
+		} else {
+			session()->flash('danger', '很抱歉，您的邮箱和密码不匹配！');
+			return redirect()->back();
+		}
+	}
+	
+	public function destroy()
+	{
+		Auth::logout();
+		return redirect()->route('home');
+	}
+	
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
-    //
+	/**
+	 * __construct
+	 *
+	 * Access managment
+	 */
+	public function __construct()	
+	{
+		$this->middleware('auth', [
+			'except' => ['create', 'store']
+		]);
+
+		$this->middleware('guest', [
+			'only' => ['create']
+		]);	
+	}
+		
+	public function create()
+	{
+		return view('users.create');
+	}
+	
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name' => 'required|max:50',
+			'email' => 'required|email|unique:users|max:255',
+			'institute' => 'required|max:255',
+			'password' => 'required|confirmed|min:6'
+		]);
+
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'institute' => $request->institute,
+			'password' => bcrypt($request->password),
+		]);
+
+		Auth::login($user);
+		return redirect()->route('home');
+	}
+	
+	public function index()
+	{
+		$admin = User::find(1);
+		$this->authorize('index', $admin);
+		$users = User::all();
+
+		return view('users.index', compact('users');
+	}
+	
+	public function update(User $user, Request $request)
+	{
+		$user->update([
+			'editable' => intval($request->editable),
+			'position' => $request->position,
+		]);
+
+		return redirect()->route('users.index');
+	}
+	
 }
