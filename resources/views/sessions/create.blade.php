cf6209639abb4b2a633d9f0c57155270aeaefb75
Hybin <hhb8550@live.com>
2018-09-09 14:23:25 +0800

    Advance Search[View Complete

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -12,10 +12,10 @@
 				{{ csrf_field() }}
 				<input type="text" name="email" class="form-control" value="{{ old('email') }}" placeholder="郵箱">
 				<input type="password" name="password" class="form-control" value="{{ old('password') }}" placeholder="密碼">
-				<label style="float: left;"><input type="checkbox" name="remember">記住我</label>
+				<label style="float: left;"><input type="checkbox" name="remember" style="margin-right: 0.5em;">記住我</label>
 				<br>
 				<button type="submit" id="submit">登錄</button>
-				<a href="{{ route('back') }}" id="cancel">返回</a>
+				<a href="/" id="cancel">返回</a>
 			</form>
 			<hr>
 			<p>還沒有賬號？<a href="{{ route('signup') }}">現在註冊</a></p>
--- resources/views/static_pages/advance.blade.php
+++ resources/views/static_pages/advance.blade.php
@@ -2,54 +2,50 @@
 @section('title', '高級搜索')
 
 @section('content')
-	<div class="jumbotron">
+	<div class="jumbotron" id="advance-search-page">
 		<h3>高級搜索</h3>
 		<form method="GET" action="{{ route('search.advance') }}" id="advance-search">
-			<label for="rhm-stats">按音韻地位查詢</label><br>
-			<div id="rhm-stats">
-				<input type="text" name="initial" placeholder="声母" class="bar" id="initial">
-				<input type="text" name="final" placeholder="韻母" class="bar" id="final">
-				<select name="kaihe" class="selection" id="kaihe">
-					<option disabled selected>開合...</option>
-					<option>開</option>
-					<option>合</option>
-				</select>
-				<select name="deng" class="selection" id="deng">
-					<option disabled selected>等...</option>
-					<option>一</option>
-					<option>二</option>
-					<option>三</option>
-					<option>四</option>
-				</select>
-				<select name="tone" class="selection" id="tone">
-					<option disabled selected>声調...</option>
-					<option>平</option>
-					<option>上</option>
-					<option>去</option>
-					<option>入</option>
-				</select>
-				<input type="text" name="tail" placeholder="攝" class="bar" id="tail">
-			</div>
-			<label for="others">按其他條件查詢</label><br>
-			<div id="others">
-				<input type="text" name="shengfu" placeholder="声符" class="bar" id="shengfu">
-				<input type="text" name="yunbu" placeholder="韻部" class="bar" id="yunbu">
-				<input type="text" name="ipa" placeholder="擬音（國際音標）" class="bar" id="ipa">
-			</div>
-			<label for="range">設定查詢範圍</label><br>
+			<p id="rhm-stats" class="marks">按音韻地位查詢</p>
+			<input type="text" name="initial" placeholder="声母" class="bar" id="initial"><br>
+			<input type="text" name="final" placeholder="韻母" class="bar" id="final"><br>
+			<input type="text" name="tail" placeholder="攝" class="bar" id="tail"><br>
+			<select name="kaihe" class="selection" id="kaihe">
+				<option disabled selected>開合...</option>
+				<option>開</option>
+				<option>合</option>
+			</select>
+			<select name="deng" class="selection" id="deng">
+				<option disabled selected>等...</option>
+				<option>一</option>
+				<option>二</option>
+				<option>三</option>
+				<option>四</option>
+			</select>
+			<select name="tone" class="selection" id="tone">
+				<option disabled selected>声調...</option>
+				<option>平</option>
+				<option>上</option>
+				<option>去</option>
+				<option>入</option>
+			</select>
+			<p id="others" class="marks">按其他條件查詢</p>
+			<input type="text" name="shengfu" placeholder="声符" class="bar" id="shengfu"><br>
+			<input type="text" name="yunbu" placeholder="韻部" class="bar" id="yunbu"><br>
+			<input type="text" name="ipa" placeholder="擬音（國際音標）" class="bar" id="ipa"><br>
+			<p id="range" class="marks">設定查詢範圍</p>
 			<div id="range">
-				<input checked type="checkbox" name="onomatope[]" value="wl" class="checkbox" id="wl">
+				<input type="checkbox" name="onomatope[]" value="wl" class="checkbox" id="wl">
                 <label for="wl">王力</label>
-                <input checked type="checkbox" name="onomatope[]" value="lfg" class="checkbox" id="lfg">
+                <input type="checkbox" name="onomatope[]" value="lfg" class="checkbox" id="lfg">
                 <label for="lfg">李方桂</label>
-                <input checked type="checkbox" name="onomatope[]" value="byp" class="checkbox" id="byp">
+                <input type="checkbox" name="onomatope[]" value="byp" class="checkbox" id="byp">
                 <label for="byp">白一平</label>
-                <input checked type="checkbox" name="onomatope[]" value="byps" class="checkbox" id="byps">
+                <input type="checkbox" name="onomatope[]" value="byps" class="checkbox" id="byps">
                 <label for="byps">白一平-沙加尔</label>
-                <input checked type="checkbox" name="onomatope[]" value="zzsf" class="checkbox" id="zzsf">
+                <input type="checkbox" name="onomatope[]" value="zzsf" class="checkbox" id="zzsf">
 				<label for="zzsf">郑张尚芳</label>
 			</div>
-			<button type="submit" id="submit">查詢</div>
+			<button type="submit" id="submit">查詢</button>
 			<a href="/" id="back">返回</a>
 		</form>
 	</div>
